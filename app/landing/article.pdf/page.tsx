38d972f71ac349f8d579646e34db1b3c1f3d0c8f
Surya Uthkarsha <suryauthkarsha@gmail.com>
2026-04-16 16:59:33 +0530

    Initial commit from Flake AI

---
+++ app/landing/article.pdf/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Landingarticlepdf() {
+  return (
+    <div className="bg-[rgb(40,40,40)] text-[rgb(0,0,0)] text-[16px] relative mx-auto overflow-hidden min-h-screen box-border w-[1280px] h-[0px]"></div>
+  );
+}
