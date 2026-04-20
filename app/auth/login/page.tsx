f09e658b12e2fb2d272da0e32987575297c53720
Surya Uthkarsha <suryauthkarsha@gmail.com>
2026-04-20 15:26:14 +0530

    Initial commit from Flake AI

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+
+
+export default function Authlogin() {
+  return (
+    <div className="bg-[rgb(0,0,0)] text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] relative mx-auto overflow-hidden min-h-screen box-border w-[1265px] h-[800px]">
+      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[0px] box-border m-0 w-[1265px] h-[800px]">
+        <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[0px] box-border m-0 w-[1265px] h-[800px]">
+          <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] p-[40px_0px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[0px] box-border m-0 w-[1265px] h-[800px]">
+            <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[40px] box-border m-0 w-[1265px] h-[0px]"></div>
+            <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] z-[9999] absolute left-[16px] top-[16px] box-border m-0 w-[1233px] h-[768px]"></div>
+            <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[40px] box-border m-0 w-[1265px] h-[535.046875px]">
+              <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[408.5px] top-[32px] box-border m-0 w-[448px] h-[503.046875px]">
+                <div className="bg-[rgb(9,9,11)] text-[rgb(250,250,250)] text-[16px] leading-[24px] p-[8px_0px] rounded-[8px] border-[1px_solid_rgb(39,_39,_42)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_2px_0px] absolute left-[0px] top-[0px] box-border m-0 w-[448px] h-[55.046875px]">
+                  <button className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[16px] top-[8px] box-border m-0 w-[66px] h-[37.046875px]">
+                    <img src="https://vid.ai/assets/logos/VIDAI_Blue_Transparent.svg" alt="Img" className="text-[rgba(0,0,0,0)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[8px] top-[8px] box-border m-0 w-[50px] h-[21.046875px]" />
+                  </button>
+                </div>
+                <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[63.046875px] box-border m-0 w-[448px] h-[440px]">
+                  <div className="bg-[rgb(9,9,11)] text-[rgb(250,250,250)] text-[16px] leading-[24px] rounded-[8px] border-[1px_solid_rgb(39,_39,_42)] shadow-[rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0)_0px_0px_0px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_2px_0px] absolute left-[0px] top-[0px] box-border m-0 w-[448px] h-[440px]">
+                    <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] p-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[0px] box-border m-0 w-[446px] h-[126px]">
+                      <p className="text-[rgb(219,234,254)] text-[24px] font-[600] leading-[32px] tracking-[-0.6px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[24px] top-[24px] box-border m-0 w-[398px] h-[32px] overflow-hidden text-clip">
+                        Login
+                      </p>
+                      <p className="text-[rgb(161,161,170)] text-[14px] leading-[20px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[24px] top-[62px] box-border m-0 w-[398px] h-[40px] overflow-hidden text-clip">
+                        You can create an account here and if you already have one, we'll sign you in.
+                      </p>
+                    </div>
+                    <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] p-[0px_24px_24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[126px] box-border m-0 w-[446px] h-[92px]">
+                      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[24px] top-[0px] box-border m-0 w-[398px] h-[68px]">
+                        <p className="text-[rgb(219,234,254)] text-[14px] font-[500] leading-[20px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[0px] box-border m-0 w-[398px] h-[20px] overflow-hidden text-clip">
+                          Email
+                        </p>
+                        <div className="bg-[rgb(0,0,0)] text-[rgb(250,250,250)] text-[14px] leading-[20px] p-[8px_12px] rounded-[6px] border-[1px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[28px] box-border m-0 w-[398px] h-[40px]"></div>
+                      </div>
+                    </div>
+                    <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] p-[0px_24px_24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[218px] box-border m-0 w-[446px] h-[220px]">
+                      <button className="bg-[rgb(219,234,254)] text-[rgb(24,24,27)] text-[14px] font-[500] leading-[20px] text-center whitespace-nowrap p-[8px_16px] rounded-[6px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[24px] top-[0px] box-border m-0 w-[398px] h-[40px]">
+                        Send Magic Link
+                      </button>
+                      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[24px] top-[64px] box-border m-0 w-[398px] h-[20px]">
+                        <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] absolute left-[0px] top-[9.5px] box-border m-0 w-[184.171875px] h-[1px]"></div>
+                        <p className="text-[rgb(209,213,219)] text-[14px] leading-[20px] p-[0px_8px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[184.171875px] top-[0px] box-border m-0 w-[29.671875px] h-[20px] overflow-hidden text-clip">
+                          or
+                        </p>
+                        <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] absolute left-[213.84375px] top-[9.5px] box-border m-0 w-[184.15625px] h-[1px]"></div>
+                      </div>
+                      <button className="bg-[rgb(219,234,254)] text-[rgb(24,24,27)] text-[14px] font-[500] leading-[20px] text-center whitespace-nowrap p-[8px_16px] rounded-[6px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[24px] top-[100px] box-border m-0 w-[398px] h-[40px]">
+                        <div dangerouslySetInnerHTML={{ __html: `<svg aria-hidden="true" focusable="false" data-icon="google" class="mr-3 w-5 fill-white dark:fill-black" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>` }}  className="text-[rgb(24,24,27)] text-[14px] font-[500] leading-[20px] text-center whitespace-nowrap border-[0px_solid_rgb(39,_39,_42)] absolute left-[110.9375px] top-[9.515625px] box-border m-0 w-[20px] h-[20.96875px]" />
+                        <p className="text-[rgb(24,24,27)] text-[14px] font-[500] leading-[20px] text-center whitespace-nowrap absolute left-[142.9375px] top-[11px] box-border m-0 w-[144.109375px] h-[17px] overflow-hidden text-clip">
+                          Continue with Google
+                        </p>
+                      </button>
+                      <p className="text-[rgb(161,161,170)] text-[14px] leading-[20px] text-center border-[0px_solid_rgb(39,_39,_42)] absolute left-[24px] top-[156px] box-border m-0 w-[398px] h-[40px] overflow-hidden text-clip">
+                        <p className="text-[rgb(161,161,170)] text-[14px] leading-[20px] text-center absolute left-[1.515625px] top-[1px] box-border m-0 w-[249.609375px] h-[17px] overflow-hidden text-clip">
+                          By clicking continue, you agree to our
+                        </p>
+                        <button className="text-[rgb(161,161,170)] text-[14px] leading-[20px] text-center border-[0px_solid_rgb(39,_39,_42)] absolute left-[255.0625px] top-[1px] box-border m-0 w-[112.765625px] h-[17px]">
+                          Terms of Service
+                        </button>
+                        <p className="text-[rgb(161,161,170)] text-[14px] leading-[20px] text-center absolute left-[371.765625px] top-[1px] box-border m-0 w-[24.71875px] h-[17px] overflow-hidden text-clip">
+                          and
+                        </p>
+                        <button className="text-[rgb(161,161,170)] text-[14px] leading-[20px] text-center border-[0px_solid_rgb(39,_39,_42)] absolute left-[151px] top-[21px] box-border m-0 w-[95.984375px] h-[17px]">
+                          Privacy Policy.
+                        </button>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[800px] box-border m-0 w-[0px] h-[0px]">
+        <p className="text-[rgb(250,250,250)] text-[16px] leading-[24px] whitespace-nowrap border-[0px_none_rgb(250,_250,_250)] absolute left-[-1px] top-[-801px] box-border m-0 w-[1px] h-[1px] overflow-hidden text-clip"></p>
+      </div>
+      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[800px] box-border m-0 w-[0px] h-[0px]">
+        <div className="text-[rgb(29,31,39)] text-[16px] leading-[18.4px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[0px] box-border m-0 w-[1265px] h-[0px]"></div>
+      </div>
+      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[800px] box-border m-0 w-[0px] h-[0px]"></div>
+      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[800px] box-border m-0 w-[0px] h-[0px]"></div>
+      <div className="text-[rgb(250,250,250)] text-[16px] leading-[24px] border-[0px_solid_rgb(39,_39,_42)] absolute left-[0px] top-[800px] box-border m-0 w-[0px] h-[0px]"></div>
+    </div>
+  );
+}
